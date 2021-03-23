979dc24f0d9b06d6d9d78d0cdd8de50fc31e0394
YQRickWang <yqrickw10@gmail.com>
2021-03-24 03:30:28 +0800

    update

--- diffuse interface methods on graphs/house voting records 1984/house_voting.m
+++ diffuse interface methods on graphs/house voting records 1984/house_voting.m
@@ -31,13 +31,19 @@ votes_array = strrep(votes_array,'?','0');
 votes_mat = str2double(votes_array);
 [n,m] = size(votes_mat);
 
+%n is the number of datapoints, m is the number of features
+
 %create weigt matrix
 Weight = zeros(n,n);
 for x=1:n
     for y=x:n
         %calculate weight
-        Weight(x,y) = exp(-norm(votes_mat(x,:)-votes_mat(y,:))^2/tau);
-        Weight(y,x) = Weight(x,y);
+        if x~=y
+            Weight(x,y) = exp(-norm(votes_mat(x,:)-votes_mat(y,:))^2/tau);
+            Weight(y,x) = Weight(x,y);
+        else
+            Weight(x,y) = 0;
+        end
     end
 end
 
@@ -49,10 +55,11 @@ end
 Degree = diag(degree_vec);
 
 Laplacian = Degree - Weight;
-D_sqrt_inv = sqrtm(Degree);
-sym_Laplacian = D_sqrt_inv*Laplacian*D_sqrt_inv;
+D_sqrt_inv = pinv(sqrtm(Degree));
+sym_Laplacian = eye(n)-D_sqrt_inv*Weight*D_sqrt_inv;
 
 %eigendecomposition
+%[V,D] = eig(Laplacian);
 [V,D] = eig(sym_Laplacian);
 
 %only keeps the real values
@@ -70,35 +77,45 @@ lambda(1)=1;lambda(2)=1;lambda(3)=1;lambda(4)=1;lambda(5)=1;
 %convex splitting
 
 %the vectors store the final value
-a=zeros(n,1);
-b=zeros(n,1);
-d=zeros(n,1);
+a=zeros(n,M+1);
+b=zeros(n,M+1);
+d=zeros(n,M+1);
 D1 = zeros(n,1);
 
+
+
+
 %intialization
-for k=1:20
-    a(k) = dot(u(:,1),V(:,k));
-    b(k) = dot(u(:,1).^3,V(:,k));
-    d(k) = 0;
+for k=1:n
+    a(k,1) = dot(u(:,1),V(:,k));
+    b(k,1) = dot(u(:,1).^3,V(:,k));
+    d(k,1) = 0;
     D1(k) = 1+dt*(epsilon*D(k,k)+c);
 end
 
-for k=1:20
-    %iterate
-    for x=1:M
-        a_next = 1/D1(k)*((1+dt/epsilon+c*dt)*a(k)-dt/epsilon*b(k)-dt*d(k));
+for x=1:M
+    for k=1:n
+        a_next = 1/D1(k)*((1+dt/epsilon+c*dt)*a(k,x)-dt/epsilon*b(k,x)-dt*d(k,x));
         %update a
-        a(k) = a_next;
-        u(:,x+1)= sum(a.*V,2);
+        a(k,x+1) = a_next;
         b_next = dot(u(:,x+1).^3,V(:,k));
         d_next = dot(lambda.*(u(:,x+1)-u(:,1)),V(:,k));
         %update
-        b(k) = b_next;
-        d(k) = d_next;
+        b(k,x) = b_next;
+        d(k,x) = d_next;
     end
+    u(:,x+1)= sum(a(:,x+1).*V,2);
 end
 
 
+class_data = data(:,1);
+class_array = table2array(class_data);
+class_array = strrep(class_array,'republican','-1');
+class_array = strrep(class_array,'democrat','1');
+class = str2double(class_array);
+
+%accuracy
+accuracy = sum(class==sign(u(:,end)))/n;
 
 
 
